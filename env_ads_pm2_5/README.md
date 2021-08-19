161d74ba758a1373237d0316f733982d3c138da0
Erick Otenyo <otenyo.erick@gmail.com>
2021-08-19 22:37:03 +0300

    Initial commit

---
+++ env_ads_pm2_5/README.md
@@ -0,0 +1,25 @@
+## Particulate Matter 2.5 Forecast Dataset Near Real-time Script
+
+This file describes the near real-time script that retrieves and processes
+the [the {dataset formal name}]({learn more link})
+for [display on EA Hazards Watch]({link to dataset's metadata page on Hazards Watch}).
+
+{Describe how the original data came from the source.}
+
+{Describe the steps used to process the data, e.g., "convert variable X from the original netcdf file for ingestion in
+GSKY."}
+
+Please see the [Python script]({link to Python script on Github}) for more details on this processing.
+
+You can view the processed {EA Hazards Watch public title}
+dataset [on Hazards Watch]({link to dataset's metadata page on Resource Watch}).
+
+You can also download the original dataset [from the source website]({download from source link}).
+
+**Schedule**
+
+This script is run {frequency of execution}. The exact time that the script is run to update the dataset can be found in
+the the `time.cron` file. This time is in Coordinated Universal Time (UTC), expressed in cron format.
+
+###### Note: This script was originally written by [{name}](mailto:{email}), and is currently maintained by [{name}](mailto:{email}).
+ 
---
+++ env_ads_pm2_5/contents/src/__init__.py
@@ -0,0 +1,50 @@
+import logging
+import sys
+import os
+
+import cdsapi
+import urllib3
+import datetime
+
+urllib3.disable_warnings()
+
+# Copernicus Atmospheric Data Store (ADS) API URL
+COPERNICUS_ADS_URL = "https://ads.atmosphere.copernicus.eu/api/v2"
+
+# Copernicus Atmospheric Data Store API KEY
+COPERNICUS_ADS_KEY = os.getenv("COPERNICUS_ADS_KEY")
+
+# Absolute path where the Data will be downloaded to
+DATA_DIR = "/data"
+
+# Variable we are interested in from Copernicus ADS
+VARIABLE = "particulate_matter_2.5um"
+
+# are of Interest bbox
+AOI_BBOX = [-12.5, 21, 24, 52]  # IGAD region
+
+
+def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    logging.info('STARTING')
+
+    client = cdsapi.Client(url=COPERNICUS_ADS_URL, key=COPERNICUS_ADS_KEY)
+
+    date_today_str = datetime.date.today().strftime("%Y-%m-%d")
+
+    file_path = f"{DATA_DIR}/{date_today_str}.nc"
+
+    request = {
+        "dataset": "cams-global-atmospheric-composition-forecasts",
+        "options": {
+            'variable': f'{VARIABLE}',
+            'date': f'{date_today_str}',
+            'time': '00:00',
+            'leadtime_hour': ['0', '24', '48', '72', '96', '120'],
+            'type': 'forecast',
+            'format': 'netcdf',
+            'area': AOI_BBOX
+        }
+    }
+
+    client.retrieve(request.get("dataset"), request.get("options"), file_path)
