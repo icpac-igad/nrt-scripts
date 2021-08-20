803a320a0b844e9538432413e33ee2aac036bc15
Erick Otenyo <otenyo.erick@gmail.com>
2021-08-20 09:33:35 +0300

    Add option for mountng host volume when running container

--- env_ads_pm2_5/start.sh
+++ env_ads_pm2_5/start.sh
@@ -3,6 +3,13 @@
 #Change the NAME variable with the name of your script
 NAME=$(basename $(pwd))
 LOG=${LOG:-udp://localhost}
+HOST_DATA_VOL=${HOST_DATA_VOL:-}
 
 docker build -t $NAME --build-arg NAME=$NAME .
-docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
+
+if [ -z "$HOST_DATA_VOL" ]; then
+  echo "Running container without host mounted volume. Data will not be persisted"
+  docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
+else
+  docker run -v $HOST_DATA_VOL:/opt/nrt-script/data --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
+fi
