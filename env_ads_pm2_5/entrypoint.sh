56272e179d399885bf9b8c3d355f29556944378a
Erick Otenyo <otenyo.erick@gmail.com>
2021-10-03 17:16:35 +0300

    user custom user for container

---
+++ env_ads_pm2_5/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 9999 as default
+CURRENT_UID=${UID:-9999}
+
+# Notify user about the UID selected
+echo "Current UID : $CURRENT_UID"
+
+# Create user  with selected UID
+useradd --shell /bin/bash -u "$CURRENT_UID" -o -c "" -m "$NAME"
+
+# Set "HOME" ENV variable for user's home directory
+export HOME=/home/"$NAME"
+
+chown -R "$NAME": /opt/"$NAME"/data
+
+# Execute process
+exec /usr/local/bin/gosu "$NAME" "$@"
--- env_ads_pm2_5/start.sh
+++ env_ads_pm2_5/start.sh
@@ -5,11 +5,7 @@ NAME=$(basename $(pwd))
 LOG=${LOG:-udp://localhost}
 HOST_DATA_VOL=${PM25_DATA_VOL:-}
 
-docker build -t $NAME --build-arg NAME=$NAME .
+docker build -t "$NAME" --build-arg NAME="$NAME" .
 
-if [ -z "$HOST_DATA_VOL" ]; then
-  echo "Running container without host mounted volume. Data will not be persisted"
-  docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
-else
-  docker run -v $HOST_DATA_VOL:/opt/$NAME/data -v $HOST_DATA_VOL:/opt/$NAME/data --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
-fi
+docker run -v "$HOST_DATA_VOL":/opt/"$NAME"/data -v "$HOST_DATA_VOL":/opt/"$NAME"/data --log-driver=syslog \
+  --log-opt syslog-address="$LOG" --log-opt tag="$NAME" --env-file .env --rm "$NAME" python main.py
